360e06fec69564d98bffccd59e4ed02054546bea
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2019-04-16 06:55:38 +0200

    Initial setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Main Part'
+    title: 'Main Page'
   });
 });
 
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').parse();
+// }
+
 const express = require('express');
-const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const mongoose = require('mongoose');
 const config = require('./database/database');
 
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(config.database, {
-  useNewUrlParser: true
-});
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
+mongoose.connect(config.database);
 const db = mongoose.connection;
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
-const router = require('./routes/index')
-app.use('/', router);
+db.on('error', error => console.log(error));
+db.once('open', () => console.log(`Connected to mongoose...`));
 
 const PORT = process.env.PORT | 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
